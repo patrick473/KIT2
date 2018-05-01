c4fbbe4ec8cd80c4f9ad40c4a52d8380815b0a49
patrick kottman <patkottman@gmail.com>
2018-05-01 14:35:11 +0200

    added landing page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,16 +10,18 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-  <!-- <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">-->
-
-    <!-- Styles -->
-   <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet">-->
-    <link href="{{ asset('css/bootstrap.min.css')}}" rel="stylesheet">
+   
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+     
+         <!-- Styles -->
+        
+         <link href="{{ asset('css/bootstrap.min.css')}}" rel="stylesheet">
+         <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+         rel="stylesheet">
+         <link href="{{ asset('css/main.css')}}" rel="stylesheet">
+   
 </head>
 <body>
     <div id="app">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
@@ -14,7 +14,10 @@
       <!-- Styles -->
      
       <link href="{{ asset('css/bootstrap.min.css')}}" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
       <link href="{{ asset('css/main.css')}}" rel="stylesheet">
+
 
 
     </head>
@@ -24,7 +27,7 @@
 
             <a class="navbar-brand " href="#">
                     <img src="{{ URL::asset('images/hu-logo.png') }}" width="30" height="30" class="d-inline-block align-top" alt="HU Logo">
-                    Kennis Instrument Toetsprogramma
+                    Kwaliteit Instrument Toetsprogramma
             </a>
 
             <!-- Opens and closes extendible link list on mobile -->
@@ -58,21 +61,125 @@
         </ul>
     </div>
             </nav>
-       
-            <div class="content">
-                    
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+       <!-- End of navbar start of content -->
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+            <div class="jumbotron text-center">
+                <h1>Kwaliteit Instrument Toetsprogramma 2.0</h1>
+                <p>De tool om uw Toetsprogramma te evalueren</p>
+            </div>
+
+            <!-- about app container -->
+            <div id="aboutkit" class="container-fluid">
+            <div class="row">
+                <div class="col-sm-8">
+                    <h2>Kwaliteit Instrument Toetsprogramma</h2><br>
+                    <h4>Give me some of your food give me some of your food give me some of your food meh, 
+                    i don't want it stare at wall turn and meow stare at wall some more meow again continue staring show belly lies down</h4><br>
+                    <p>Cat ipsum dolor sit amet, plop down in the middle where everybody walks hiding behind the couch until lured out by a feathery toy.
+                        Knock dish off table head butt cant eat out of my own dish milk the cow stick butt in face scream at teh bath run outside as soon as door open. 
+                        Grab pompom in mouth and put in water dish. 
+                        Stare at ceiling light sniff catnip and act crazy,
+                        but behind the couch, but spend six hours per day washing,
+                    </p><br>
+                    <button class="btn btn-default btn-lg">Meer informatie</button>
+                   
+                </div>
+                <div class="col-sm-4">
+                        <i class="material-icons" id="firstIcon">feedback</i>
                 </div>
             </div>
+            </div>
+            <!-- about company container-->
+            <div id="aboutcompany" class="container-fluid bg-grey">
+            <div class="row">
+                    <div class="col-sm-4">
+                            <i class="material-icons" id="secondIcon">business</i>
+                    </div>
+                    <div class="col-sm-8">
+                            <h2>Kwaliteit Instrument Toetsprogramma</h2><br>
+                            <h4>Give me some of your food give me some of your food give me some of your food meh, 
+                            i don't want it stare at wall turn and meow stare at wall some more meow again continue staring show belly lies down</h4><br>
+                            <p>Cat ipsum dolor sit amet, plop down in the middle where everybody walks hiding behind the couch until lured out by a feathery toy.
+                                Knock dish off table head butt cant eat out of my own dish milk the cow stick butt in face scream at teh bath run outside as soon as door open. 
+                                Grab pompom in mouth and put in water dish. 
+                                Stare at ceiling light sniff catnip and act crazy,
+                                but behind the couch, but spend six hours per day washing,
+                            </p><br>
+                            <button class="btn btn-default btn-lg">Meer informatie</button>
+                           
+                        </div>
+
+            </div>
+
+            </div>
+
+            <!-- what is offered section -->
+            <div id="services" class="container-fluid text-center">
+            <h2> services</h2>
+            <h4>Wat bieden wij</h4>
+            <br>
+            <div class="row slideanim">
+                    <div class="col-sm-4">
+                            <i class="material-icons md-96" id="">business</i>
+                        <h4>POWER</h4>
+                        <p>Lorem ipsum dolor sit amet..</p>
+                    </div>
+                    <div class="col-sm-4">
+                            <i class="material-icons md-96" id="">business</i>
+                        <h4>POWER</h4>
+                        <p>Lorem ipsum dolor sit amet..</p>
+                    </div>
+                    <div class="col-sm-4">
+                            <i class="material-icons md-96" id="">business</i>
+                        <h4>POWER</h4>
+                        <p>Lorem ipsum dolor sit amet..</p>
+                    </div>
+
+                </div>
+                <br><br>
+                <div class="row slideanim">
+                <div class="col-sm-4">
+                        <i class="material-icons md-96" id="">business</i>
+                        <h4>POWER</h4>
+                        <p>Lorem ipsum dolor sit amet..</p>
+                    </div>
+                    <div class="col-sm-4">
+                            <i class="material-icons md-96" id="">business</i>
+                        <h4>POWER</h4>
+                        <p>Lorem ipsum dolor sit amet..</p>
+                    </div>
+                    <div class="col-sm-4">
+                            <i class="material-icons md-96" id="">business</i>
+                        <h4>POWER</h4>
+                        <p>Lorem ipsum dolor sit amet..</p>
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- Contact -->
+
+            <div id="contactsection" class="container-fluid bg-grey">
+                <h2 class="text-center">Contact</h2>
+                <div class="row">
+                    <div class="col-sm-5">
+                        <p >We proberen zo snel mogelijk contact op te nemen met u.</p>
+                       
+
+                        
+                        <p > <i class="material-icons " id="postalIcon">email</i> Postbus 14007,3508 SB, Utrecht</p>
+                        <p > <i class="material-icons " id="emailIcon">message</i> <a href="mailto:lectoraatberoepsonderwijs@hu.nl">lectoraatberoepsonderwijs@hu.nl</a></p>
+                        <p id="phonePart"> <i class="material-icons " id="phoneIcon">local phone</i> <a href="callto:088 - 481 13 43">088 - 481 13 43</a></p>
+                        
+                    </div>
+                </div>
+
+            </div>
+            </div>
+
+
+            
+
         
 
            <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
