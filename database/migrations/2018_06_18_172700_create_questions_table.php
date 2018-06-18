94befccc21fdd5048e93d4248b499eae7a3e2b63
patrick kottman <patkottman@gmail.com>
2018-06-18 19:36:36 +0200

    updated migrations

--- database/migrations/2018_06_18_172700_create_questions_table.php
+++ database/migrations/2018_06_18_172700_create_questions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSurveysTable extends Migration
+class CreateQuestionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,13 @@ class CreateSurveysTable extends Migration
      */
     public function up()
     {
-        Schema::create('surveys', function (Blueprint $table) {
+        Schema::create('questions', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('survey_id');
+            $table->foreign('survey_id')->references('id')->on('surveys')->onDelete('cascade'); 
+            $table->string('type');
             $table->string('title');
-            $table->string('description');
+            $table->json('attributes');
             $table->timestamps();
         });
     }
@@ -28,6 +31,6 @@ class CreateSurveysTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('surveys');
+        Schema::dropIfExists('questions');
     }
 }
