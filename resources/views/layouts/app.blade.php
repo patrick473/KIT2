28fbb307e44e1441ba97ef907fbbd4216fbfb25a
Dannymos <dannymoswow@gmail.com>
2018-06-24 22:13:17 +0200

    Added slider

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Kwaliteits Isntrument Toetsprogramma | Hogeschool Utrecht') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
    
         <!-- Fonts -->
--- resources/views/survey/detail.blade.php
+++ resources/views/survey/detail.blade.php
@@ -5,6 +5,8 @@
     <link href="{{ asset('css/survey.css')}}" rel="stylesheet">
     <script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{asset('js/ui/jquery-ui.min.js')}}"></script>
+    <link href="{{ asset('js/ui/jquery-ui.min.css')}}" rel="stylesheet">
 
     <div class="card col-xs-12">
         <div class="card-body">
@@ -58,6 +60,7 @@
                             <label class="input-label col-xs-12 col-md-3">Type vraag: </label>
                             <select id="question-type-input" class="form-control survey-input survey-select col-xs-12 col-md-6" name="type" required autofocus>
                                 <option>Text</option>
+                                <option>Slider</option>
                             </select>
                         </div>
 
