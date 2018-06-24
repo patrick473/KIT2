9c62088111c51b5379a89f162cd320fc3969072a
thomasboy020 <thomasboy020@gmail.com>
2018-06-25 01:05:04 +0200

    slider werkt

--- public/js/answersurvey.js
+++ public/js/answersurvey.js
@@ -40,18 +40,33 @@ function fetch_survey_data(query = '') {
 
     $.each(data.questions,(i,e)=>{
 
-    document.getElementById('body').innerHTML +="<br><p>Vraag: " + e.title + "</p>";
-    document.getElementById('body').innerHTML +="<p>Beschrijving: " + e.description + "</p>";
-    document.getElementById('body').innerHTML +="<p>Type: " + e.type + "</p>";
-
     if (e.type == "Text") {
         $('#body').append("<textarea> </textarea><br>");
     }
     if (e.type == "Slider") {
-        $('#body').append("");
+        $('#body').append(`<div class="row">
+          <div class="col-md-12">
+            <h5>`+e.title+`</h5>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <p>`+e.description+`</p>
+          </div>
+        </div>
+        <div class='row'>
+          <div class="col-md-12">
+            <div class="slider-wrapper row">
+                <input class='col-md-12 col-xs-12 slider' type='range'/>
+                <input disabled value=`+e.attributes.start+` class='autosave col-md-4 col-xs-4 slider-start start'/><input disabled value=`+e.attributes.middle+` class='autosave slider-middle col-md-4 col-xs-4 middle'/><input disabled value=`+e.attributes.end+` class='autosave slider-end col-md-4 col-xs-4 end'/>
+            </div>
+          </div>
+        </div>
+
+          </div>`);
     }
     if (e.type == "Radio") {
-        $('#body').append("<input type=radio></input><br>");
+        $('#body').append(`<input type=radio value=`+e.attributes.first+`></input><br>`);
         $('#body').append("<input type=radio></input><br>");
         $('#body').append("<input type=radio></input><br>");
         $('#body').append("<input type=radio></input><br>");
