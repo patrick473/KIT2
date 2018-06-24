8428d87b8d79d13dae8e562e6689cd77a95a7107
fir3d3vil <timvdlooy@gmail.com>
2018-06-24 22:13:39 +0200

    damn thats some admin shit

--- public/js/adminSurveyOverview.js
+++ public/js/adminSurveyOverview.js
@@ -3,21 +3,11 @@ $(document).on("click",".addSurvey",(e)=>{
         location.href = "/admin/selectsurvey/"+group_id;
 });
 
-$(document).on("click",".surveyAnswers",(e)=>{
-        let survey_id = $('#surveyAnswers').data("survey_id");
-        location.href = "/group/surveyAnswers/"+survey_id;
-});
-
-$(document).on("click",".answerSurvey",(e)=>{
-        let group_id = $("#addSurvey").data("group_id");
-        location.href = "/group/answer/"+group_id;
-});
-
 $(document).delegate(".removeSurvey_group","click",(e)=>{
     let survey_id = $("#removeSurvey_group").data("survey_id");
     let group_id = $("#addSurvey").data("group_id");
 
-    let url = "/api/survey/surveyGroup/"+survey_id+"/"+group_id;
+    let url = "/api/admin/surveyGroup/"+survey_id+"/"+group_id;
     $.ajax({
         type: "GET",
         url: url,
