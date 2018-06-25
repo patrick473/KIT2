067416bf982a548f5b32660b91666aec22a70a99
patrick kottman <patkottman@gmail.com>
2018-06-25 04:31:14 +0200

    yuf

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -14,6 +14,11 @@ use Log;
 use App\survey_group;
 class SurveyController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
       public function surveyAnswers($id){
         $app = app();
         //get variables
